0b9cb1e1b35081f87e4895a8013198ca4d8b9524
snkoski <shawnnkoski@gmail.com>
2018-08-27 11:35:49 -0400

    setup server.js file, run on port 3001

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const Chatkit = require('pusher-chatkit-server')
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:cfcce473-08a0-4edb-a20e-a92ed1cba1e3',
+  key: '80ad39be-dc76-40fd-9d57-e1f3359dd204:6lo87h38Rz4UuS2C/eWT8Ypr8XHkvRlsHGltFpfikaY='
+})
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/users', (req, res) => {
+  const { username } = req.body
+  const user = { name: username, id: username }
+  chatkit
+    .createUser(user)
+    .then(() => {
+      console.log('Created user', user.name)
+      res.status(201).json(user)
+    })
+    .catch(error => {
+      if (error.error === 'services/chatkit/user_already_exists') {
+        console.log('User already exists ', user.name)
+        res.status(201).json(user)
+      } else {
+        console.error(error)
+        res.status(error.status).json(error)
+      }
+    })
+})
+
+app.listen(3001)
+console.log('Running on port 3001');
